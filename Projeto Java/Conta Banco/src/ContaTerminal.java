a38fbc722744bf996440a4fea650bdf1318b2ce5
RoselangeSoares <roselange_boll@hotmail.com>
2023-09-21 01:31:24 -0300

    primeiro projeto java conta terminal

--- Projeto Java/Conta Banco/src/ContaTerminal.java
+++ Projeto Java/Conta Banco/src/ContaTerminal.java
@@ -1,8 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main (String[] args){
+        Scanner scanner = new Scanner (System.in).useLocale(Locale.US);
+
+        String saldo = "R$ " + 237 + "," + 48;
+
+        System.out.println("Olá, seja bem-vindo, pressione ENTER para abertura de conta ");
+        String ENTER = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu nome! (depois ENTER para prosseguir) ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome! (depois ENTER para prosseguir) ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite o número da Agência! (depois ENTER para prosseguir) ");
+        String agencia = scanner.next();
+
+        System.out.println("E por fim digite o número da conta! (depois ENTER para FINALIZAR) ");
+        int num = scanner.nextInt();
+
+        System.out.println("Olá, " + nome  + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + num + " e seu saldo " + saldo + " já esta disponível para saque. ");
     }
-
-    
-
 }
